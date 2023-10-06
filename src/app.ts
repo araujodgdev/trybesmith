ae9057822a68fbcc72f446dc7a1a2543894e09ea
Douglas Araújo <devdoug45@gmail.com>
2023-10-06 01:14:17 -0300

    feat: é possível cadastrar um produto sem autenticação em POST /products

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 
 import productsRouter from './routes/products.router';
-import handleAuth from './middlewares/auth.middleware';
 import loginRouter from './routes/login.router';
 
 const app = express();
 
 app.use(express.json());
 app.use(loginRouter);
-app.use(handleAuth);
 app.use(productsRouter);
 
 export default app;
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -4,14 +4,10 @@ import productsServices from '../Services/products.services';
 import mapHttpStatus from '../util/mapHTTPStatus';
 
 async function registerNewProduct(req: Request, res: Response) {
-  try {
-    const registerResponse = await productsServices.register(req.body);
-    const { status, data } = registerResponse;
+  const registerResponse = await productsServices.register(req.body);
+  const { status, data } = registerResponse;
 
-    res.status(mapHttpStatus(status)).json(data);
-  } catch (error) {
-    res.status(500).json({ message: 'Internal error!' });
-  }
+  res.status(mapHttpStatus(status)).json(data);
 }
 
 export default {
